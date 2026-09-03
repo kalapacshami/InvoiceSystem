1813c22bb8125a2bdf42390756d577c3b41c41ed
kalapacshami <marcell.hammer@gmail.com>
2026-09-03 19:04:49 +0200

    OrderItem class is done

--- InvoiceSystem.Domain/Entities/OrderItem.cs
+++ InvoiceSystem.Domain/Entities/OrderItem.cs
@@ -8,5 +8,13 @@ namespace InvoiceSystem.Domain.Entities
 {
     public class OrderItem
     {
+        public int Id { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPriceAtOrderTime { get; set; }
+        public int OrderId { get; set; }
+        public Order Order { get; set; } = null!;
+        public int ProductId { get; set; }
+        public Product Product { get; set; } = null!;
+
     }
 }
