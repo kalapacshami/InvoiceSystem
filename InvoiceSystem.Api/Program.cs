a5c0e603bcb71b5cbf9b31e7f13aa9047a86a96e
kalapacshami <marcell.hammer@gmail.com>
2026-09-03 19:59:50 +0200

    Started working on PDF generation

--- InvoiceSystem.Api/Program.cs
+++ InvoiceSystem.Api/Program.cs
@@ -25,7 +25,7 @@ namespace InvoiceSystem.Api
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-
+            QuestPDF.Settings.License = QuestPDF.Infrastructure.LicenseType.Community;
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
