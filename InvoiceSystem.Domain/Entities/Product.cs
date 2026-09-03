599b6668920f0a0ac6e24be2127d644848486001
kalapacshami <marcell.hammer@gmail.com>
2026-09-03 18:57:04 +0200

    Product class is done

---
+++ InvoiceSystem.Domain/Entities/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvoiceSystem.Domain.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Category { get; set; } = string.Empty;
+        public decimal UnitPrice { get; set; }
+        public bool IsHazardous { get; set; }
+        public bool IsDiscountEligible { get; set; }
+
+
+    }
+}
