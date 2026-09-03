aa543d05b69fa0f9ec0cde36272932d2c9aa712f
kalapacshami <marcell.hammer@gmail.com>
2026-09-03 19:00:35 +0200

    Customer class is done

---
+++ InvoiceSystem.Domain/Entities/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvoiceSystem.Domain.Entities
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public ICollection<Order> Orders { get; set; } = new List<Order>();
+    }
+}
