4f2ee678b369d504937a0409808c7de256147197
kalapacshami <marcell.hammer@gmail.com>
2026-09-03 19:09:34 +0200

    DbContect class added

---
+++ InvoiceSystem.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using InvoiceSystem.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvoiceSystem.Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; } = null!;
+        public DbSet<Customer> Customers { get; set; } = null!;
+        public DbSet<Order> Orders { get; set; } = null!;
+        public DbSet<OrderItem> OrderItems { get; set; } = null!;
+    }
+}
