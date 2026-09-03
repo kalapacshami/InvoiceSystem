84588bd4f57f809691c8c50598e941e8971d5c9a
kalapacshami <marcell.hammer@gmail.com>
2026-09-03 19:02:40 +0200

    Order class is done

--- InvoiceSystem.Domain/Entities/Order.cs
+++ InvoiceSystem.Domain/Entities/Order.cs
@@ -8,5 +8,10 @@ namespace InvoiceSystem.Domain.Entities
 {
     public class Order
     {
+        public int Id { get; set; }
+        public DateTime OrderDate { get; set; }
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; } = null!;
+        public ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
     }
 }
