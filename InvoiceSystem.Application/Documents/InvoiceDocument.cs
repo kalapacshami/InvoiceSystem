29d50bfc09d0727ef2383526f5b40fac6103b16a
kalapacshami <marcell.hammer@gmail.com>
2026-09-03 20:03:09 +0200

    Working on Compose, Header part is done

--- InvoiceSystem.Application/Documents/InvoiceDocument.cs
+++ InvoiceSystem.Application/Documents/InvoiceDocument.cs
@@ -1,10 +1,13 @@
 ﻿using InvoiceSystem.Application.Dtos;
+using QuestPDF.Fluent;
+using QuestPDF.Helpers;
 using QuestPDF.Infrastructure;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace InvoiceSystem.Application.Documents
 {
@@ -19,7 +22,39 @@ namespace InvoiceSystem.Application.Documents
         public DocumentMetadata GetMetadata() => DocumentMetadata.Default;
         public void Compose(IDocumentContainer container)
         {
-            throw new NotImplementedException();
+            container.Page(page =>
+            {
+                page.Margin(30);
+                page.Size(PageSizes.A4);
+                page.DefaultTextStyle(x => x.FontSize(11));
+
+                page.Header().Element(ComposeHeader);
+                page.Content().Element(ComposeContent);
+                page.Footer().AlignCenter().Text(x =>
+                {
+                    x.CurrentPageNumber();
+                    x.Span(" / ");
+                    x.TotalPages();
+                });
+            });
+        }
+        private void ComposeHeader(IContainer container)
+        {
+            container.Row(row =>
+            {
+                row.RelativeItem().Column(column =>
+                {
+                    column.Item().Text("INVOICE").FontSize(20).Bold();
+                    column.Item().Text($"Order #{_order.Id}");
+                    column.Item().Text($"Date: {_order.OrderDate:yyyy-MM-dd}");
+                });
+
+                row.RelativeItem().AlignRight().Column(column =>
+                {
+                    column.Item().Text("Customer").Bold();
+                    column.Item().Text(_order.CustomerName);
+                });
+            });
         }
     }
 }
