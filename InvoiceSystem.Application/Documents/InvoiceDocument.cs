eaee41c48a5f475ee7f8dee2b2be4f9ec93d0af8
kalapacshami <marcell.hammer@gmail.com>
2026-09-03 20:01:28 +0200

    working on PDF generation

--- InvoiceSystem.Application/Documents/InvoiceDocument.cs
+++ InvoiceSystem.Application/Documents/InvoiceDocument.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using InvoiceSystem.Application.Dtos;
+using QuestPDF.Infrastructure;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,18 @@ using System.Threading.Tasks;
 
 namespace InvoiceSystem.Application.Documents
 {
-    public class InvoiceDocument
+    public class InvoiceDocument : IDocument
     {
+        private readonly OrderResponse _order;
+        public InvoiceDocument(OrderResponse order)
+        {
+            _order = order;
+        }
+
+        public DocumentMetadata GetMetadata() => DocumentMetadata.Default;
+        public void Compose(IDocumentContainer container)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
